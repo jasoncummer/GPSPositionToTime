ed134defa07eacefa9c33865c64f613a0853c5c4
jason cummer <jasoncummer@gmail.com>
2012-11-21 13:01:32 -0800

    layout fixes

--- gen/com/gpspositiontotime/R.java
+++ gen/com/gpspositiontotime/R.java
@@ -15,19 +15,19 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btnShowLocation=0x7f070001;
+        public static final int btnShowLocation=0x7f070003;
         public static final int degreeTextView=0x7f070008;
-        public static final int gpsDegreesNumberPicker=0x7f070005;
+        public static final int gpsDegreesNumberPicker=0x7f070007;
         public static final int gpsMinutesNumberPicker=0x7f070006;
-        public static final int gpsNumberPicker=0x7f070007;
-        public static final int longitudeTextView=0x7f07000b;
+        public static final int gpsSecondsNumberPicker=0x7f070005;
+        public static final int longitudeTextView=0x7f07000a;
         public static final int menu_settings=0x7f07000c;
-        public static final int minutestextView=0x7f070009;
-        public static final int secondsTextView=0x7f07000a;
+        public static final int minutestextView=0x7f07000b;
+        public static final int secondsTextView=0x7f070009;
         public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070002;
-        public static final int textView3=0x7f070004;
-        public static final int utc_textView=0x7f070003;
+        public static final int textView2=0x7f070001;
+        public static final int textView3=0x7f070002;
+        public static final int utc_textView=0x7f070004;
     }
     public static final class layout {
         public static final int activity_android_gpspos_to_time=0x7f030000;
